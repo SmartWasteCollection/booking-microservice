e095665a1bf632ac71ef23034e0ecd05cc13a31c
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2022-07-05 17:36:41 +0200

    chore: add enum in booking model

--- src/models/bookingModel.js
+++ src/models/bookingModel.js
@@ -1,14 +1,21 @@
 module.exports = function(mongoose) {
-    const Schema = mongoose.Schema;
-    const BookingSchema = new Schema({
-        id_booking:  String,
-        id_user: String,
-        type_of_waste: String,
-        datetime: Date,
-        city: String,
-        province: String,
-        address: String,
-        status: String,
-    });
-    return mongoose.model('Booking', BookingSchema);
+	const Schema = mongoose.Schema;
+	const BookingSchema = new Schema({
+		id_booking:  String,
+		id_user: String,
+		type_of_waste: {
+			type: String,
+			enum: ["TWIGS", "WASTE OIL", "IRON", "ELECTRONICS", "CLOTHES", "OTHER"]
+		},
+		datetime: Date,
+		city: String,
+		province: String,
+		address: String,
+		status: {
+			type: String,
+			enum: ["PENDING", "ASSIGNED", "COMPLETED"],
+			default: "PENDING"
+		},
+	});
+	return mongoose.model("Booking", BookingSchema);
 };
