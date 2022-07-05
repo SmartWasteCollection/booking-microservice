dff075e340c723ef69a277c954e69bafd1499d42
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2022-07-05 13:14:43 +0200

    chore: fix mongoose connection callback

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose')
 var cors = require('cors')
 var path = require('path');
 
+var Booking = require('./src/models/bookingModel.js')(mongoose)
+
 global.appRoot = path.resolve(__dirname);
 
 var PORT = 3000;
@@ -12,11 +14,23 @@ app.use(cors())
 app.use(express.json());
 app.use('/static', express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost:27017/swc.bookings',
+mongoose.connect('mongodb://localhost:27017/swc-bookings',
     {useNewUrlParser: true, useUnifiedTopology: true},
     (e) => {
-    console.log("Error on connect");
+    if(e == null){
+        console.log("Connected to mongoDB");
+    }else{
+        e();
+    }
 });
+
+app.get("/bookings", (req, res) => {
+    Booking.find({}, function(err, bookings) {
+        if (err)
+            res.send(err);
+        res.json(bookings);
+    });
+})
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello!")
